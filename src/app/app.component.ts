3beaf4f7f21accd70742c0ea8f863d9aa907b1ac
leidy alvarez <alvarezgarcialeidy@artesyletras.edu.co>
2023-04-11 21:38:48 -0500

    Introduccion angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto';
+  title:string = 'Leidy';
+  edad:number = 24;
+  pasaLaMateria:boolean = true;
+  dato:any = 65;
+  contador:number = 0;
+  
+  aumentar(){
+    this.contador=this.contador + 1;
+  }
+
+  disminuir(){
+    this.contador=this.contador - 1;
+  }
+
+  multiplicar(){
+    this.contador=this.contador * 2;
+  }
+
+  dividir(){
+    this.contador=this.contador / 2;
+  }
+
+  resetear(){
+    this.contador= 0;
+  }
+
+  esPar(numero:number):boolean{
+    if(numero %2 === 0){
+      return true
+      
+    }else{
+      return false
+    }
+    
+  }
+
+
+  estudiantes:string[]=["Nathalie", "Walter", "Alejandro", "Romario", "Luisa", "Daniela"];
+
+
 }
